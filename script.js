0859ac13ba5b4e4d145f3bca3ae91f45bec3e91f
Blaise <ifollowjre@gmail.com>
2020-01-07 19:22:30 -0700

    appending tests test lol

--- script.js
+++ script.js
@@ -30,7 +30,101 @@ $(document).ready(function () {
         }).then(function (res) {
             console.log(res);
             //// this is where the fun is ////
-            
+            var date1 = (res.list[5].dt_txt).split("-").join("/");
+            var date2 = (res.list[13].dt_txt).split("-").join("/");
+            var date3 = (res.list[21].dt_txt).split("-").join("/");
+            var date4 = (res.list[29].dt_txt).split("-").join("/");
+            var date5 = (res.list[37].dt_txt).split("-").join("/");
+
+            var temp1 = (res.list[5].main.temp_max + '°F');
+            var temp2 = (res.list[13].main.temp_max + '°F');
+            var temp3 = (res.list[21].main.temp_max + '°F');
+            var temp4 = (res.list[29].main.temp_max + '°F');
+            var temp5 = (res.list[37].main.temp_max + '°F');
+
+            var humid1 = ('Humidity ' + res.list[5].main.humidity + '%');
+            var humid2 = ('Humidity ' + res.list[13].main.humidity + '%');
+            var humid3 = ('Humidity ' + res.list[21].main.humidity + '%');
+            var humid4 = ('Humidity ' + res.list[29].main.humidity + '%');
+            var humid5 = ('Humidity ' + res.list[37].main.humidity + '%');
+
+            var wind1 = ('Wind Speed ' + res.list[5].wind.speed + 'Mph')
+            var wind2 = ('Wind Speed ' + res.list[13].wind.speed + 'Mph')
+            var wind3 = ('Wind Speed ' + res.list[21].wind.speed + 'Mph')
+            var wind4 = ('Wind Speed ' + res.list[29].wind.speed + 'Mph')
+            var wind5 = ('Wind Speed ' + res.list[37].wind.speed + 'Mph')
+            // var windSpeed = (res.wind.speed);
+            // console.log(tempDay1);
+            var icon1 = res.list[6].weather[0].icon;
+            var icon2 = res.list[14].weather[0].icon;
+            var icon3 = res.list[22].weather[0].icon;
+            var icon4 = res.list[30].weather[0].icon;
+            var icon5 = res.list[38].weather[0].icon;
+
+            var weatherIcon1 = "http://openweathermap.org/img/wn/" + icon1 + "@2x.png"
+            var weatherIcon2 = "http://openweathermap.org/img/wn/" + icon2 + "@2x.png"
+            var weatherIcon3 = "http://openweathermap.org/img/wn/" + icon3 + "@2x.png"
+            var weatherIcon4 = "http://openweathermap.org/img/wn/" + icon4 + "@2x.png"
+            var weatherIcon5 = "http://openweathermap.org/img/wn/" + icon5 + "@2x.png"
+
+
+            console.log(res);
+
+
+            function weatherApp1() {
+                $("#weatherBox1").empty();
+
+                $("#weatherBox1").append("<h1 class='changeMeDate'>" + date1 + "</h1>");
+                $("#weatherBox1").append("<img class='imgChange' src='" + weatherIcon1 + "'>");
+                $("#weatherBox1").append("<p class='changeMe1'>" + temp1 + "</p>");
+                $("#weatherBox1").append("<p class='changeMe1'>" + humid1 + "</p>");
+                $("#weatherBox1").append("<p class='changeMe1'>" + wind1 + "</p>");
+            }
+            function weatherApp2() {
+                $("#weatherBox2").empty();
+
+                $("#weatherBox2").append("<h1 class='changeMeDate'>" + date2 + "</h1>");
+                $("#weatherBox2").append("<img class='imgChange' src='" + weatherIcon2 + "'>");
+                $("#weatherBox2").append("<p class='changeMe1'>" + temp2 + "</p>");
+                $("#weatherBox2").append("<p class='changeMe1'>" + humid2 + "</p>");
+                $("#weatherBox2").append("<p class='changeMe1'>" + wind2 + "</p>");
+
+            }
+            function weatherApp3() {
+                $("#weatherBox3").empty();
+
+                $("#weatherBox3").append("<h1 class='changeMeDate'>" + date3 + "</h1>");
+                $("#weatherBox3").append("<img class='imgChange' src='" + weatherIcon3 + "'>");
+                $("#weatherBox3").append("<p class='changeMe1'>" + temp3 + "</p>");
+                $("#weatherBox3").append("<p class='changeMe1'>" + humid3 + "</p>");
+                $("#weatherBox3").append("<p class='changeMe1'>" + wind3 + "</p>");
+
+            }
+            function weatherApp4() {
+                $("#weatherBox4").empty();
+
+                $("#weatherBox4").append("<h1 class='changeMeDate'>" + date4 + "</h1>");
+                $("#weatherBox4").append("<img class='imgChange' src='" + weatherIcon4 + "'>");
+                $("#weatherBox4").append("<p class='changeMe1'>" + temp4 + "</p>");
+                $("#weatherBox4").append("<p class='changeMe1'>" + humid4 + "</p>");
+                $("#weatherBox4").append("<p class='changeMe1'>" + wind4 + "</p>");
+
+            }
+            function weatherApp5() {
+                $("#weatherBox5").empty();
+
+                $("#weatherBox5").append("<h1 class='changeMeDate'>" + date5 + "</h1>");
+                $("#weatherBox5").append("<img class='imgChange' src='" + weatherIcon5 + "'>");
+                $("#weatherBox5").append("<p class='changeMe1'>" + temp5 + "</p>");
+                $("#weatherBox5").append("<p class='changeMe1'>" + humid5 + "</p>");
+                $("#weatherBox5").append("<p class='changeMe1'>" + wind5 + "</p>");
+
+            }
+            weatherApp1();
+            weatherApp2();
+            weatherApp3();
+            weatherApp4();
+            weatherApp5();
 
 
 
